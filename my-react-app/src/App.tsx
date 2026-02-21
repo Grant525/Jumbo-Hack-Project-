f15f0cd7fc75cecbb89717c59e3193092933e146
AJ Romeo <ajromeo7226@gmail.com>
2026-02-21 18:46:39 -0500

    route

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -46,7 +46,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/ " replace />} />
       </Routes>
     </BrowserRouter>
   );
