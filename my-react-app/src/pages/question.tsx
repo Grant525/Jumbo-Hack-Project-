2eecf4ed90ec04c81757e827406a6dc5c4895b9d
Grant525 <grantcaile0@gmail.com>
2026-02-21 16:42:16 -0500

    Code editor in the each question p2

--- my-react-app/src/pages/question.tsx
+++ my-react-app/src/pages/question.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
 import questionsData from "../data/questions.json";
+import CodeEditor from "../components/CodeEditor";
+import React from "react";
+
 
 export interface Question {
   id: number;
@@ -10,17 +14,27 @@ export interface Question {
   starter_code_prompt: string;
 }
 
-const questions: Question[] = questionsData.questions as Question[];
-
-export default questions;
+const questions: Question[] = questionsData as unknown as Question[];
 
 export function groupByChapter(qs: Question[]): Record<string, Question[]> {
-  return qs.reduce(
-    (acc, q) => {
-      if (!acc[q.chapter]) acc[q.chapter] = [];
-      acc[q.chapter].push(q);
-      return acc;
-    },
-    {} as Record<string, Question[]>,
+  return qs.reduce((acc, q) => {
+    if (!acc[q.chapter]) acc[q.chapter] = [];
+    acc[q.chapter].push(q);
+    return acc;
+  }, {} as Record<string, Question[]>);
+}
+
+export default function QuestionPage() {
+  const { id } = useParams();
+  const question = questions.find((q) => q.id === Number(id));
+
+  if (!question) return <div>Question not found</div>;
+
+  return (
+    <div>
+      <h1>{question.title}</h1>
+      <p>{question.description}</p>
+      <CodeEditor language="python" />
+    </div>
   );
 }
