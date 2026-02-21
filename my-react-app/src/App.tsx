9921326231c44408a74b447b2811f6465cde4193
AJ Romeo <ajromeo7226@gmail.com>
2026-02-21 18:07:33 -0500

    test

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,19 +3,41 @@ import Home from "./pages/home";
 import Lessons from "./pages/lessons";
 import Question from "./pages/question";
 import SignIn from "./pages/sign-in";
-import Settings from "./pages/settings";
 import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/lessons" element={<ProtectedRoute><Lessons /></ProtectedRoute>} />
-        <Route path="/question/:id" element={<ProtectedRoute><Question /></ProtectedRoute>} />
-        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+
+        {/* {Protected} */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/lessons"
+          element={
+            <ProtectedRoute>
+              <Lessons />
+            </ProtectedRoute>
+          }
+        />
+        {/* <Route
+          path="/question/:id"
+          element={
+            <ProtectedRoute>
+              <Question />
+            </ProtectedRoute>
+          }
+        /> */}
+        <Route path="*" element={<Navigate to="/sign-in" replace />} />
       </Routes>
     </BrowserRouter>
   );
