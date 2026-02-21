03ed9c760f40017e18620e7d50ac54c33a9e762a
cschob01 <carlos.schober@tufts.edu>
2026-02-21 14:48:43 -0500

    Fixing key

--- my-react-app/src/supabase.js
+++ my-react-app/src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://cqkvwrslybwnmxtuxzni.supabase.co",
-  "https://jumbo-hack-project.vercel.app/"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxa3Z3cnNseWJ3bm14dHV4em5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTEyNDksImV4cCI6MjA4NzI2NzI0OX0.Cz9yuEMENywXWUvqrW26LcR7W1fmsOpp2_iqB5FBsNo"
 );
