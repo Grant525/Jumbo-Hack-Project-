b40d1908059f52cc1c469c1e75314bc0a10c7f3a
AJ Romeo <ajromeo7226@gmail.com>
2026-02-22 14:41:21 -0500

    logo

--- my-react-app/src/pages/lessons.tsx
+++ my-react-app/src/pages/lessons.tsx
@@ -81,7 +81,7 @@ export default function Lessons() {
 
       <header className="lessons-header">
         <div className="lessons-logo">
-          <span className="lessons-logo-icon">{"</>"}</span>
+          <span className="lessons-logo-icon">{"◈"}</span>
           <span className="lessons-logo-text">Rosetta</span>
         </div>
         <nav className="lessons-nav centered-nav">
